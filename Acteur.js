717acbd384ab05e96ea362f9a9095901c2fca36c
dzdrs <40268657+dzdrs@users.noreply.github.com>
2018-12-05 15:00:29 +0100

    ajout des colision

--- Acteur.js
+++ Acteur.js
@@ -6,6 +6,10 @@
             this.x = x;
             this.y = y;
 
+            //Création et initialisation des attributs pour la largeur et la hauteur
+            this.width = 32;
+            this.height = 32;
+
             //création d'un objet de type HtmlElementImage
             this.img = document.createElement("IMG");
 
@@ -16,6 +20,21 @@
             this.img.style.position = "absolute";
             this.img.style.left = this.x;
             this.img.style.top = this.y;
+
+            //Cette méthode renvoie true si l'acteur est en collision avec celui référencé par l'argument unActeur
+            isCollisioningwith()
+            {
+                //Cas ou unActeur est à gauche du bord de this
+                if(unActeur.x + unActeur.width < this.x) return false;
+                //Cas ou unActeur est à droit du bord de this
+                if(unActeur.x > unActeur.width + this.x) return false;
+                //Cas ou unActeur est au dessus du bord superieur de this
+                if(unActeur.y + unActeur.height < this.y) return false;
+                //Cas ou unActeur est en dessous du bord inférieur de this
+                if(unActeur.y > unActeur.height + this.y) return false;
+                //Le reste des cas ne sont que des collisons
+                return true;
+            }
 
             //insère l'images dans le terrain de jeu
             var terrain = document.getElementById("TerrainDeJeu");
