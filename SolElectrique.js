2b1bc287019c3c7bf564fc304a584486737ea5b3
dzdrs <40268657+dzdrs@users.noreply.github.com>
2019-02-06 11:57:31 +0100

    solelectrique

---
+++ SolElectrique.js
@@ -0,0 +1,8 @@
+class SolElectrique extends Acteur
+{
+    constructor( x, y )
+    {
+        // Appel du constructeur de la classe mère
+        super( "images/solElectrique.png", x, y ) ;
+    }
+}
